10e4ba46c87a084b97cde58c21e5dd20608f1ff4
freckie <freckie@frec.kr>
2021-07-26 13:48:59 +0900

    Update: the created-at

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -8,7 +8,7 @@ export const ThumbnailItem = ({ node }) => (
   <Link className={`thumbnail visible ${TARGET_CLASS}`} to={node.fields.slug}>
     <div key={node.fields.slug}>
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <p>{node.frontmatter.date} 작성</p>
+      <p class="created-at">{node.frontmatter.date} 작성</p>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
     </div>
   </Link>
