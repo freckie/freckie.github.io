e65560f1e05bf483f76efa441c8b6245e3f85464
freckie <freckie@frec.kr>
2025-12-05 14:00:08 +0900

    style: figcaption

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import remarkGfm from 'remark-gfm';
 import remarkMath from 'remark-math';
+import remarkFigureCaption from '@microflash/remark-figure-caption';
 import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
@@ -12,7 +13,7 @@ export default defineConfig({
   site: 'https://blog.frec.kr',
   integrations: [mdx(), sitemap()],
   markdown: {
-    remarkPlugins: [remarkGfm, remarkMath],
+    remarkPlugins: [remarkGfm, remarkMath, remarkFigureCaption],
     rehypePlugins: [rehypeKatex],
   },
 });
